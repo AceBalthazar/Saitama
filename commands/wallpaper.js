9eb394d42116d2d2833964f0c5ec5eaff0284c42
AceBalthazar <brotherhoodbrown130@gmail.com>
2023-03-23 11:08:05 -0400

    -updated comments on wallpaper command -updated comments on WallpaperFilePicker function

--- commands/wallpaper.js
+++ commands/wallpaper.js
@@ -1,26 +1,35 @@
+//	embed builder and attachment builder are required for uploading images to discord within an embed
+//	ImageMetadata and AverageColor are used to flesh out the embed with more information related to the image, such as resolution and the average color
 const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
 const chosenFile = require('../functions/WallpaperFilePicker.js');
 const { getImageMetadata } = require('../functions/WallpaperExifData.js');
 const { getAverageColor } = require('../functions/WallpaperColorThief.js');
 //	TODO: Need to account for images that are larger than 8 MB, as the bot will be unable to embed them if they are
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('wallpaper')
 		.setDescription('responds with a random wallpaper from the database'),
 	async execute(interaction) {
+		//	chosenFile function will be re-written in the future, may need to change code here to account for rewrite
 		const WallpaperFile = chosenFile.chosenFile();
+		//	defines the entire filepath of the image, so i do not have to type out the whole path every time it is used
 		const filepath = `./wallpapers/${WallpaperFile}`;
 
-
+		//	grabs full EXIF Metadata of the selected image
+		//	consult the exiftool-vendored and exiftool sites for more info related to available metadata: https://github.com/ikmolbo/exiftool-vendored
 		const metadata = await getImageMetadata(filepath);
+		//	logging to console kept in for testing, commented out for production
 		//	console.log(metadata);
 
-
+		//	grabs average color of the selected image
+		//	consult Lokesh Dhakar's site for more information about color thief:  https://lokeshdhakar.com/projects/color-thief/
 		const averageColor = await getAverageColor(filepath);
 		//	console.log(averageColor);
 
 
 		const file = new AttachmentBuilder(filepath);
+		//	consult discord.js guide docs for further information related to embed builders: https://discordjs.guide/popular-topics/embeds.html
 		const WallpaperEmbed = new EmbedBuilder()
 			.setTitle(WallpaperFile)
 			.setColor(averageColor)
--- functions/WallpaperFilePicker.js
+++ functions/WallpaperFilePicker.js
@@ -5,6 +5,10 @@
 
 //	TODO: set a filter so this will only return photo filetypes compatible with discord, like .png and .jpg
 //	This would also prevent the embeds from breaking due to incorrect file type. Current solution is only put .png's into the folder.
+
+//	TODO: rework how this file is formatted to match the syntax of other functions used by the bot.
+
+
 const fs = require('fs');
 const path = require('node:path');
 
